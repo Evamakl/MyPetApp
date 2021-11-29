af9d0b32cad52da9e2be45ca2ecc02844d923b87
hadar shushan <hadar5840@gmail.com>
2021-11-29 21:23:39 +0200

    הוספת בדיקות יחידה

--- app/src/main/java/com/example/myproject/new_user.java
+++ app/src/main/java/com/example/myproject/new_user.java
@@ -8,9 +8,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class new_user extends AppCompatActivity {
 
+    TextView password;
+    TextView email;
+    TextView name;
+    TextView phone;
     Button end;
     Button goBack;
 
@@ -19,6 +28,11 @@ public class new_user extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        password = (TextView) findViewById(R.id.password);
+        email = (TextView) findViewById(R.id.email);
+        name = (TextView) findViewById(R.id.username);
+        phone = (TextView) findViewById(R.id.phone);
 
         end = (Button) findViewById(R.id.button_end);
         end.setOnClickListener(new View.OnClickListener() {
@@ -41,14 +55,43 @@ public class new_user extends AppCompatActivity {
 
     }
 
+
+
     public void openNewPage() {
         Intent intent = new Intent(this, End.class);
         startActivity(intent);
     }
 
     public void returnBack() {
-       Intent intent = new Intent(this, Exist_new_frame.class);
-       startActivity(intent);
+        Intent intent = new Intent(this, Exist_new_frame.class);
+        startActivity(intent);
+    }
+
+    public static boolean isValidPassword(String password) {
+        final String PASSWORD_PATTERN =
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+        final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean isValidPhoneNumber(String phone){
+        return  !(!phone.matches("(00972|0|\\+972)[5][0-9]{8}") && !phone.matches("(00970|0|\\+970)[5][0-9]{8}") && !phone.matches("(05[0-9]|0[12346789])([0-9]{7})") && !phone.matches("(00972|0|\\+972|0|)[2][0-9]{7}"));
+    }
+
+    public static boolean isAlpha(String name) {
+        String expression = "^[a-zA-Z]*$";
+        CharSequence inputStr = name;
+        Pattern pattern = Pattern.compile(expression);
+        Matcher matcher = pattern.matcher(inputStr);
+        return matcher.matches();
     }
 
 }
--- app/src/test/java/com/example/myproject/ExampleUnitTest.java
+++ app/src/test/java/com/example/myproject/ExampleUnitTest.java
@@ -1,17 +1,45 @@
 package com.example.myproject;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import com.example.myproject.new_user;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
+    //@Test
+    //public void addition_isCorrect() { assertEquals(4, 2 + 2); }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void isAlpha_test() {
+        assertTrue(new_user.isAlpha("hadar"));
+        assertFalse(new_user.isAlpha("ha123"));
+        assertFalse(new_user.isAlpha("123"));
+    }
+
+    @Test
+    public void isValidPassword_test() {
+        assertFalse(new_user.isValidPassword("12345"));
+        assertFalse(new_user.isValidPassword("1234ab"));
+        assertFalse(new_user.isValidPassword("123456789"));
+
+    }
+
+    @Test
+    public void isEmailValid_test() {
+        assertFalse(new_user.isEmailValid("hadar"));
+        assertFalse(new_user.isEmailValid("hadar@hadar"));
+        assertTrue(new_user.isEmailValid("hadar@hadar.com"));
+    }
+
+    @Test
+    public void isValidPhoneNumber_test() {
+        assertFalse(new_user.isValidPhoneNumber("123456789"));
+        assertTrue(new_user.isValidPhoneNumber("0523567122"));
     }
 }
+
