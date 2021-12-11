5252d8dddd28e09b385c8c9cfe4b387f6a7710c0
hadar shushan <hadar5840@gmail.com>
2021-12-11 21:43:45 +0200

    תיקון

--- app/src/main/java/com/example/myproject/MainActivity1.java
+++ app/src/main/java/com/example/myproject/MainActivity1.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
         Intent intent = new Intent(this, new_user.class);
         startActivity(intent);
     }
