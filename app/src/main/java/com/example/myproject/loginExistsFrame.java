eb8170efacbbcd4f92d32e2d4f5d77f9af36b35c
EvaMariaK <93390175+EvaMariaK@users.noreply.github.com>
2021-12-13 17:07:29 +0200

    עבודה על דאטה בייס

--- app/src/main/java/com/example/myproject/loginExistsFrame.java
+++ app/src/main/java/com/example/myproject/loginExistsFrame.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,6 +17,9 @@ public class loginExistsFrame extends AppCompatActivity implements View.OnClickL
 
     EditText etUserName, etPassword;
     Button btnSignIn, btnBack;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
