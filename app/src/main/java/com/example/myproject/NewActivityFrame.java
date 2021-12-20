c0e61df6928807e9519be9e4d13cffb781376753
ester <esterm8297@gmail.com>
2021-12-20 18:08:15 +0200

    הוספת הגדרה של התנתקות, והתחברות כמשתמש קיים

--- app/src/main/java/com/example/myproject/NewActivityFrame.java
+++ app/src/main/java/com/example/myproject/NewActivityFrame.java
@@ -2,7 +2,10 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NewActivityFrame extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class NewActivityFrame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_frame);
+    }
+
+    public void signOut(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
     }
 }
---
+++ app/src/main/java/com/example/myproject/googleCalendar.java
@@ -0,0 +1,15 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class googleCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_calendar);
+    }
+}
