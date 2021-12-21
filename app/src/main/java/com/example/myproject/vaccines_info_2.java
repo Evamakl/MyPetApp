3228847a7655bb9c83c66ba757866a098aaaf9f5
ester <esterm8297@gmail.com>
2021-12-21 10:12:26 +0200

    ידיעון-חיסונים

---
+++ app/src/main/java/com/example/myproject/vaccines_info_2.java
@@ -0,0 +1,32 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class vaccines_info_2 extends AppCompatActivity {
+    Button back;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vaccines_info2);
+
+        back = (Button) findViewById(R.id.backbt);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openNewActivityNew();
+            }
+        });
+    }
+    public void openNewActivityNew() {
+        Intent intent = new Intent(this, vaccines_info.class);
+        startActivity(intent);
+    }
+}
