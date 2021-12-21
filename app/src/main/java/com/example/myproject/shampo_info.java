55c1904b52308d40396bbb63a9dd3003f21d2a46
ester <esterm8297@gmail.com>
2021-12-21 10:50:29 +0200

    ידיעון-מקלחות וטיולים

--- app/src/main/java/com/example/myproject/shampo_info.java
+++ app/src/main/java/com/example/myproject/shampo_info.java
@@ -2,13 +2,30 @@ package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class shampo_info extends AppCompatActivity {
-
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shampo_info);
+        back = (Button) findViewById(R.id.backbt);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openNewActivityBack();
+            }
+        });
+    }
+
+    public void openNewActivityBack() {
+        Intent intent = new Intent(this, search_page.class);
+        startActivity(intent);
     }
 }
