42130148e9f9b360d67355f120ad4b00312d22ae
ShilatTsfoni <93181174+ShilatTsfoni@users.noreply.github.com>
2021-12-22 21:55:34 +0200

    יצירת עמוד מנהל מערכת+עדכון גרסה

---
+++ app/src/main/java/com/example/myproject/CreateFeedbackMess.java
@@ -0,0 +1,74 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateFeedbackMess extends AppCompatActivity {
+
+    //Initialize variable
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_feedback_mess);
+
+        //Assign variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view) {
+        //Open drawer
+        HomePageManager.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo(View view) {
+        //Close drawer
+        HomePageManager.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view) {
+        //Redirect activity to home
+        HomePageManager.redirectActivity(this,HomePageManager.class);
+    }
+
+    public void ClickReports(View view) {
+        //Redirect activity to reports
+        HomePageManager.redirectActivity(this, Reports.class);
+    }
+
+    public void ClickNewsletterUpdate(View view) {
+        //Redirect activity to newsletter update
+        HomePageManager.redirectActivity(this, NewsletterUpdate.class);
+    }
+
+    public void ClickCreateFeedbackMess(View view) {
+        //Recreate activity
+        recreate();
+    }
+
+    public void ClickBlockingUser(View view) {
+        //Redirect activity to blocking user
+        HomePageManager.redirectActivity(this, BlockingUser.class);
+    }
+
+    public void ClickAppUpdate(View view) {
+        //Redirect activity to app update
+        HomePageManager.redirectActivity(this, AppUpdate.class);
+    }
+
+    public void ClickLogout(View view) {
+        //Close app
+        HomePageManager.logout(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close drawer
+        HomePageManager.closeDrawer(drawerLayout);
+    }
+}
---
+++ app/src/main/java/com/example/myproject/HomePageManager.java
@@ -0,0 +1,132 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomePageManager extends AppCompatActivity {
+
+    //Initialize variable
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_manager);
+
+        //Assign variable
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view) {
+        //Open drawer
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        //Open drawer Layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view) {
+        //Close drawer
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        //Close drawer Layout
+        //Check condition
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            //When drawer is open
+            //Close drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickHome(View view) {
+        //Recreate activity
+        recreate();
+    }
+
+    public void ClickReports(View view) {
+        //Redirect activity to reports
+        redirectActivity(this,Reports.class);
+    }
+
+    public void ClickNewsletterUpdate(View view) {
+        //Redirect activity to newsletter update
+        redirectActivity(this,NewsletterUpdate.class);
+    }
+
+    public void ClickCreateFeedbackMess(View view) {
+        //Redirect activity to create feedback mess
+        HomePageManager.redirectActivity(this, CreateFeedbackMess.class);
+    }
+
+    public void ClickBlockingUser(View view) {
+        //Redirect activity to blocking user
+        HomePageManager.redirectActivity(this, BlockingUser.class);
+    }
+
+    public void ClickAppUpdate(View view) {
+        //Redirect activity to app update
+        HomePageManager.redirectActivity(this, AppUpdate.class);
+    }
+
+    public void ClickLogout(View view){
+        //Close app
+        logout(this);
+    }
+
+    public static void logout(Activity activity) {
+        //Initialize alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        //Set title
+        builder.setTitle("Logout");
+        //Set message
+        builder.setMessage("Are you sure you want to logout?");
+        //Positive yes button
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Finish activity
+                activity.finishAffinity();
+                //Exit app
+                System.exit(0);
+            }
+        });
+        //Negative no button
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss dialog
+                dialog.dismiss();
+            }
+        });
+        //Show dialog
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity,Class aclass) {
+        //Initialize intent
+        Intent intent = new Intent(activity,aclass);
+        //Set flag
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //Start activity
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close drawer
+        closeDrawer(drawerLayout);
+    }
+}
