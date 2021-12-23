cb3a3c75843561d3670c6ba25576f05e55c5f67f
ester <esterm8297@gmail.com>
2021-12-23 21:42:21 +0200

    עדכון

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openNewActivity() {
-        Intent intent = new Intent(this,NewActivityFrame.class);
+        Intent intent = new Intent(this,Calendar.class);
         startActivity(intent);
     }
 
