74a492b146b5b61ae83101b77e5cc8d71d810593
ShilatTsfoni <93181174+ShilatTsfoni@users.noreply.github.com>
2021-12-28 13:48:08 +0200

    יצירת עמודי דוחות

--- app/src/main/java/com/example/myproject/dogsReport.java
+++ app/src/main/java/com/example/myproject/dogsReport.java
@@ -3,65 +3,20 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class Reports extends AppCompatActivity {
+public class dogsReport extends AppCompatActivity {
 
-    //Initialize variable
     DrawerLayout drawerLayout;
-    Button usersReport;
-    Button dogsReport;
-    Button feedbackReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reports);
+        setContentView(R.layout.activity_dogs_report);
 
-        //Assign variable
         drawerLayout = findViewById(R.id.drawer_layout);
 
-        usersReport = (Button) findViewById(R.id.usersReport);
-        usersReport.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openUsersReport();
-            }
-        });
-
-        dogsReport = (Button) findViewById(R.id.dogsReport);
-        dogsReport.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openDogsReport();
-            }
-        });
-
-        feedbackReport = (Button) findViewById(R.id.feedbackReport);
-        feedbackReport.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                openFeedbackReport();
-            }
-        });
-    }
-
-    public void openDogsReport (){
-        Intent intent = new Intent(this, dogsReport.class);
-        startActivity(intent);
-    }
-
-    public void openUsersReport (){
-        Intent intent = new Intent(this, UsersReport.class);
-        startActivity(intent);
-    }
-
-    public void openFeedbackReport (){
-        Intent intent = new Intent(this, FeedbackReport.class);
-        startActivity(intent);
     }
 
     public void ClickMenu(View view) {
@@ -80,8 +35,8 @@ public class Reports extends AppCompatActivity {
     }
 
     public void ClickReports(View view) {
-        //Recreate activity
-        recreate();
+        //Redirect activity to reports
+        HomePageManager.redirectActivity(this, Reports.class);
     }
 
     public void ClickNewsletterUpdate(View view) {
