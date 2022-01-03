66b6c618aa34748f5beb1cbe68eac9e864f3aea2
hadar shushan <hadar5840@gmail.com>
2022-01-03 14:29:36 +0200

    שינויים

--- app/src/test/java/com/example/myproject/Chat_Test.java
+++ app/src/test/java/com/example/myproject/Chat_Test.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class Chat_Test {
     @Test
     public void isValidMessage_test(String msg) {
-        assertFalse(msg.length() == 0); //If the message is empty is False
-        assertTrue(msg.length() > 0); //If the message bigger than 0 (so it is not empty), is True
+        assertFalse(msg.length() == 0); //If the message is empty, return False
+        assertTrue(msg.length() > 0); //If the message bigger than 0 (so it is not empty), return True
     }
 }
