3fb8f0f2931e23e60ce57c348a20ebaed80fb812
ester <esterm8297@gmail.com>
2022-01-05 20:28:52 +0200

    תיקון התפריט בדף של BlockingUser.java

--- app/src/main/java/com/example/myproject/BlockingUser.java
+++ app/src/main/java/com/example/myproject/BlockingUser.java
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -24,7 +26,7 @@ import java.util.ArrayList;
 public class BlockingUser extends AppCompatActivity {
 
     //Initialize variable
-    DrawerLayout drawerLayout;
+    DrawerLayout drawer_layout;
     private ImageView MenuIcon, BackIcon;
     NavigationView navigation;
     Intent intent;
@@ -33,40 +35,71 @@ public class BlockingUser extends AppCompatActivity {
     ArrayList<User> list;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference reference = database.getReference().child("Users");
+    private Menu menu;
+    private com.google.android.material.navigation.NavigationView NavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocking_user);
         list = new ArrayList<>();
         //Assign variable
-        drawerLayout = findViewById(R.id.drawer_layout);
+        drawer_layout = findViewById(R.id.drawer_layout);
         intent = getIntent();
         user = (User)intent.getSerializableExtra("user");
-        navigation = findViewById(R.id.NavigationView);
-        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
-                new ManagerNavigation(BlockingUser.this,item.getItemId(),user);
-                return false;
-            }
-        });
         MenuIcon = findViewById(R.id.MenuItem);
         BackIcon = findViewById(R.id.BackItem);
+        NavigationView = findViewById(R.id.NavigationView);
+        menu = NavigationView.getMenu();
+        if(user.getType().toString().equals("PetKeeper")) {
+            menu.clear();
+            new MenuInflater(this).inflate(R.menu.pet_keeper_menu, menu);
+            super.onCreateOptionsMenu(menu);
+        }
+        else if(user.getType().toString().equals("Owner")) {
+            menu.clear();
+            new MenuInflater(this).inflate(R.menu.base_activity, menu);
+            super.onCreateOptionsMenu(menu);
+        }
+        else if(user.getType().toString().equals("Manager")) {
+            menu.clear();
+            new MenuInflater(this).inflate(R.menu.manager_menu, menu);
+            super.onCreateOptionsMenu(menu);
+        }
+        menu.findItem(R.id.FullName_item).setTitle( "שלום, " + user.getUsername());
         MenuIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawerLayout.open();
+                drawer_layout.open();
             }
         });
         BackIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(BlockingUser.this, HomePageManager.class);
+                Intent intent = new Intent(BlockingUser.this, HomePageManager.class);
                 intent.putExtra("user", user);
                 startActivity(intent);
                 finish();
             }
         });
+        NavigationView.setNavigationItemSelectedListener(new com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
+                int id = item.getItemId();
+                if(user.getType().toString().equals("Manager"))
+                    new ManagerNavigation(BlockingUser.this,id,user);
+                else if(user.getType().toString().equals("PetKeeper"))
+                    new PetKeeperNavigation(BlockingUser.this,id,user);
+                else
+                    new OwnerNavigation(BlockingUser.this,id,user,item);
+                return false;
+            }
+        });
+        if(user.getType().toString().equals("Owner")) {
+            menu = NavigationView.getMenu();
+            for (int i = 0; i < user.getDogs().size(); i++)
+                menu.findItem(R.id.Dogs).getSubMenu().add(user.getDogs().get(i).getName());
+        }
+
         recyclerView=findViewById(R.id.UsersRV);
         getUsers();
     }
